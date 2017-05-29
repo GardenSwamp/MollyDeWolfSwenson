b12923d284ebe460749c12309ba07cfd0b46cde0
GardenSwamp <mathias@havemose.se>
2017-05-30 00:05:22 +0200

    second commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#second-list").click(function(){
+      $("#second-list").text("She worked in the Office of Presidential Personnel, which oversees recruitment and more.");
+  });
+  $("#third-list").click(function(){
+      $("#third-list").text("Oh, and she did this while she was working for the Obama Administration at The White House.");
+  });
+  $("#fourth-list").click(function(){
+      $("#fourth-list").text("She has developed brand strategies for celebrities like Bent Stiller, Shakira and Kobe Bryant.");
+  });
+   $("#fifth-list").click(function(){
+       $("#fifth-list").text("She was the former COO of RYOT from 2012-16 before stepping back to focus on the marketing value of the company.");
+  });
+     $("#seventh-list").click(function(){
+       $("#seventh-list").text("RYOT was bought by none other than Huffington Post for over 10 million dollars.");
+  });
+     $("#eighth-list").click(function(){
+    $("#eighth-list").text("#BIG FAN!");
+});
+  });
